4f48212b8178b996cc0ce0d6319ba7c8a5816f0a
Foram <foramdadhania16@gmail.com>
2019-09-11 19:47:00 -0400

    - TextInput created for the user -  Displaying values entered by user

--- StroopColorsDemo/App.js
+++ StroopColorsDemo/App.js
@@ -6,109 +6,100 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TextInput
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+constructor() {
+  super()
+    this.state = {
+      value: "Foram"
+    }
+    this.handleUserInput = this.handleUserInput.bind(this)
+}
 
-const App = () => {
+handleUserInput(userInput) {
+    this.setState({
+        value: userInput
+    })
+}
+
+render() {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+    <View style={styles.container}>
+    
+        <Text style={styles.userColor}> Insert your color</Text>
+    
+            <TextInput
+                defaultValue={this.state.value}
+                onChangeText={this.handleUserInput}
+            />
+            <Text> Your favourite color is {this.state.value} </Text>
+    </View>
+    );
+}
+
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
 
-export default App;
+    // flexDirection: 'row'
+    //always colomn by default
+  },
+
+    userColor: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 30,
+      color: 'blue'
+    }
+
+  // half1: {
+  //  flex: 1,
+  //  justifyContent: 'center',
+  //  alignItems: 'center',
+  //  backgroundColor: 'green'
+  // },
+
+  // half2: {
+  //  flex: 1,
+  //  flexDirection: 'row',
+  //  backgroundColor: 'blue'
+  // },
+
+  // half2x: {
+  //  flex: 1,
+  //  justifyContent: 'center',
+  //  alignItems: 'center',
+  // },
+
+  // half21: {
+  //  backgroundColor: 'orange'
+  // },
+
+  // half22: {
+  //  backgroundColor: 'yellow'
+  // },
+
+  // text: {
+  //  fontSize: 25,
+  //  color: 'black'
+  // }
+
+  
+})
+
+
+
+
+
